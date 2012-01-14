f842e9224366c9cfc2c6549fac22190c6300006b
sirex <sirexas@gmail.com>
2012-01-14 15:52:00 +0200

    Added anytorst packages to install requirements.

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='django-sboard',
       version='0.1',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+          'anytorst',
+      ])
