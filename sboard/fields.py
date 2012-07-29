7eb6ad1f7271028fb487d7cb1fd3a35f6f522623
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-07-29 16:57:37 +0300

    Make parent field in node update forms work.
    Now it accepts slugs or ids and displays slugs when possible.
    Previously it only accepted slugs, but displayed ids.

--- sboard/fields.py
+++ sboard/fields.py
@@ -6,8 +6,10 @@ from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
 from couchdbkit.client import ViewResults
+from couchdbkit.exceptions import ResourceNotFound
 
 from .models import get_node_by_slug
+from .models import couch
 from .urls import slug
 
 slug_re = re.compile(r'^%s$' % slug)
@@ -19,12 +21,27 @@ validate_slug = RegexValidator(slug_re,
 class NodeField(forms.SlugField):
     default_validators = [validate_slug]
 
+    def prepare_value(self, value):
+        if value:
+            try:
+                node = couch.get(value)
+                if node.slug:
+                    return '%s+%s' % (node.slug, node._id)
+                else:
+                    return node._id
+            except ResourceNotFound:
+                return value
+
     def clean(self, value):
         value = super(NodeField, self).clean(value)
         if value in EMPTY_VALUES:
             return None
 
-        node = get_node_by_slug(value)
+        try:
+            node = couch.get(value)
+        except ResourceNotFound:
+            node = get_node_by_slug(value)
+
         if node is None:
             raise forms.ValidationError(_("'%s' does not exists.") % value)
         elif isinstance(node, ViewResults):
